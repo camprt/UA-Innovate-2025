fa462afc0865b93422f02eb12117869c0f5ae127
Campbell Thompson <campthompson1@gmail.com>
2025-03-30 08:04:59 -0500

    updated parser

--- Parser_test.py
+++ Parser_test.py
@@ -1,44 +1,170 @@
-import hl7
-# import pyodbc
+from hl7apy.parser import parse_message
+from hl7apy.core import Group, Segment
+import mysql.connector
+from mysql.connector import Error
+import data_generator.random_names as random_names
 
-# Example HL7 Message (ADT^A01)
-hl7_message = "MSH|^~\&||HOSP_WS|||202502261022||ORU^R01^NURAS|508065.6796364|D|2.1\rEVN|A01|202502260903|||EFD.AJ^JORGENSEN^AMANDA^^^^|202502260903\rPID|1||W000033049|W23038|mairi^ghita^26^^^^L||19900505|F|^^^^^||^^^^^^^^|||||||W00000331191\rPV1|1|I|W.PBTRAIN^W.PB01^026|EM|||\rDRCPOELIVE^DRCPOE^LIVE^^^^MD|DRCPOELIVE^DRCPOE^LIVE^^^^MD|.DNK^KNOW^DOES^NOT^^^|MAS||||UNK|WI|N|DRCPOELIVE^DRCPOE^LIVE^^^^MD|IN||U|||||||||||||||||||HOSP_WS^|NEW HIRE TRAINING DOCUMENTATION|ADM|||202502260903||||||||DRCPOELIVE^DRCPOE^LIVE^^^^MD\rOBX|1||ZBBO32^Heart Rate BPM:||109\rOBX|2||ZBB1^Temperature F:||98.0\rOBX|3||AYYT888^Blood Pressure||74/109\rOBX|4|ABC|SS12345||Discussed lifestyle changes with ghita mairi to manage diabetes."
+#Establish database connection
+importedDB = None
+try:
+    importedDB = mysql.connector.connect(
+        host="sql5.freesqldatabase.com",
+        user="sql5770327",
+        password="sPhSlG51bT",
+        database="sql5770327"
+    )
+    
+    print("MySQL Database connection successful")
+except Error as err:
+        print(f"Error: '{err}'")
 
-# Parse the HL7 message
-parsed_message = hl7.parse(hl7_message)
+def xor(data, key):
+    # Make sure the key is a single character (e.g., a letter or number)
+    key = ord(key)  # Convert the key to its ASCII value (int)
+    
+    # XOR each character with the key and return the result
+    result = ''.join(chr(ord(char) ^ key) for char in data)
+    
+    return result
 
-# Extract necessary fields (this depends on the structure of your HL7 message)
-PID_need = [3, 5, 7, 8, 9, 11, 13, 18, 19]
-for i < len(parsed_message.segment('PID'))
-    PID.append(parsed_message.segment('PID')[i][0]
+def execute(connection, msg, vals):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(msg, vals)
+        connection.commit()
+    except Error as err:
+        print(f"Error: '{err}'")
+
+#MAIN: read file
+with open('source_hl7_messages_v2.hl7', 'r') as hl7_input:
+    text = hl7_input.read()
+    messages = text.split("MSH")
+    
+    # Read each line in the file
+    for line in messages[1:2]:
+        line = "MSH" + line
+        msg = parse_message(line.replace("\n", "\r"), find_groups=True, validation_level=2)
+
+        date = msg.msh.MSH_7.value
+        pid = msg.pid
+        MRN = pid.PID_3.PID_3_1.value
+        encryptedMRN = xor(MRN, 'x')
+        fake_person = random_names.generate_person(encryptedMRN, pid.PID_8.value, pid.PID_11.PID_11_4.value, int(pid.PID_7.PID_7_1.value[:4]))
+
+        #if MRN exists, add msg to tex`
+        #SELECT COUNT(*) as count FROM Program WHERE department_id = 1;
+        askMRN = "SELECT * FROM `Patient_Info` WHERE MRN = %s;"
+        cursor = importedDB.cursor()
+        record = ""
+        try:
+            cursor.execute(askMRN, (MRN,))
+            record = cursor.fetchone()
+            importedDB.commit()
+        except Error as err:
+            print(f"Error: '{err}'")
+
+        #make new record if new mrn
+        if not record:
+            print("adding new record")
+            #PID_Arr = [mrn,first_name, last_name, _birthday, sex, street_address, _city, _county, state, _phone, email_address, act_no, _ssn]pid.PID_11.PID_11_4.value, pid.PID_7.PID_7_1.value.year)
+            
+            sql = "INSERT INTO Patient_Info (`MRN`, `lastName`, `DOB`, `gender`, `alias`,  `phoneNum`, `accountNum`, `ssn`, `Message_List`, `Dr_ID`) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s);"
+            val = (MRN, fake_person[2], fake_person[3], pid.PID_8.value, pid.PID_9.value, pid.PID_13.value, pid.PID_18.value, pid.PID_19.value, line, msg.pv1.PV1_7.value, line, msg.pv1.PV1_7.value)
+            execute(importedDB, sql, val)
+            print("added record MRN: ", MRN)
+        #update
+        else:
+            print("Updating record for MRN = " + MRN)
+            sql = "UPDATE Patient_Info set `Message_List`=%s where MRN = %s;"
+            values = (str(record) + '\n' + str(line), MRN)
+            execute(importedDB, sql, values)
+        
+
+        #get object back
+        askMRN = "SELECT * FROM `Patient_Info` WHERE MRN = %s;"
+        cursor = importedDB.cursor()
+
+        try:
+            cursor.execute(askMRN, (MRN,))
+            record = cursor.fetchone()
+            importedDB.commit()
+        except Error as err:
+            print(f"Error: '{err}'")
+
+        if record:
+            #update PHI
+            #updating values
+            new_values = []
+            new_fields = []
+    
+            columns = [desc[0] for desc in cursor.description]
+            for i, col in enumerate(columns):
+                if not record[column] and col == "fullName":
+                    new_fields.append("fullName")
+                    pid5 = Field("PID_5")
+                    pid5.pid_5_1 = fake_person[1] #fname
+                    pid5.pid_5_2 = fake_person[2] #lname
+                    new_values.append(pid5)
+                
+                if not record[column] and col == "lastName":
+                    new_fields.append("lastName")
+                    new_values.append(fake_person[2])
+                if not record[column] and col =="DOB":
+                    new_fields.append("DOB")
+                    new_values.append(fake_person[3])
+                if not record[column] and col =="gender":
+                    new_fields.append("gender")
+                    new_values.append(pid.PID_8.value)
+                if not record[column] and col =="alias":
+                    new_fields.append("alias")
+                    new_values.append(pid.PID_9.value)
+                if not record[column] and col =="address":
+                    new_fields.append("address")
+                    pid11 = Field("PID_11")
+                    pid11.pid_11.pid_11_1 = fake_person[5] #street
+                    pid11.pid_11.pid_11_3 = fake_person[6] #city
+                    pid11.pid_11.pid_11_9 = fake_person[7] #count
+                    pid11.pid_11.pid_11_4 = fake_person[8] #state
+                    new_values.append(pid11.value)
+                if not record[column] and col =="phoneNum":
+                    new_fields.append("phoneNum")
+                    new_values.append(fake_person[9])
+                if not record[column] and col  =="accountNum":
+                    new_fields.append("accountNum")
+                    new_values.append(fake_person[11])
+                if record[column] and col =="ssn":
+                    new_fields.append("ssn")
+                    new_values.append(fake_person[12])
+                if str(record[column]) != msg.pv1.PV1_7.value:
+                    new_fields.append("Dr_ID")
+                    new_values.append(msg.pv1.PV1_7.value)
+    
+            #perform updates
+            if new_fields:
+                print("Updating: ", new_fields)
+                set_clause = ", ".join([f"`{field}` = %s" for field in new_fields])
+                sql = f"UPDATE `Patient_Info` SET {set_clause} WHERE `MRN` = %s;"
+                new_values.append(MRN)
+                execute(importedDB, sql, tuple(new_values))
+            
+    
+        #add msg record
+        sql = "INSERT INTO Message (MRN, Message_Text, Date) VALUES (%s, %s, %s)"
+        val = (encryptedMRN, line, date)
+        execute(importedDB, sql, val)
+
+        
+
+def execute_query(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        connection.commit()
+        print("Query successful")
+    except Error as err:
+        print(f"Error: '{err}'")
+
+
+
     
 
-
-
-
-patient_id = parsed_message.segment('PID')[3][0]  # Patient ID
-patient_name = parsed_message.segment('PID')[5][0]  # Patient Name (e.g., Doe^John)
-dob = parsed_message.segment('PID')[7][0]  # Date of Birth
-gender = parsed_message.segment('PID')[8][0]  # Gender
-alias = parsed_message.segment('PID')[9][0] # nickname
-address = parsed_message.segment('PID')[11][0]  # Address
-phone_number = parsed_message.segment('PID')[13][0]  # Phone Number
-account_number = parsed_message.segment('PID')[18][0]
-ssn = parsed_message.segment('PID')[19][0]
-
-
-# # Connect to your SQL database (example using pyodbc for SQL Server)
-# conn = pyodbc.connect('DRIVER={SQL Server};SERVER=sql5.freesqldatabase.com;DATABASE=sql5770327;UIDsql5770327;PWD=sPhSlG51bT')
-# cursor = conn.cursor()
-
-# # Insert the data into the table
-# cursor.execute("""
-#     INSERT INTO Patient_Info (MRN, Name, DOB, Gender, Alias, phoneNum, accountNum, ssn)
-#     VALUES (?, ?, ?, ?, ?, ?, ?, ?)
-#     """, 
-#     12345, patient_id, patient_name, dob, gender, address, phone_number, ethnicity, 'ADT^A01', '2023-03-29 09:00:00', 'GETDATE()'
-# )
-
-# # Commit and close
-# conn.commit()
-# conn.close()
